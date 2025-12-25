f510ebdcc8f9a89dab01504a8c3aac3c28b6d2e9
sar1yeva <yaxsisariyeva55@gmail.com>
2025-12-25 03:54:08 -0500

    network_security

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '
-/^(Registrant|Admin|Tech)/{
-  if ($1 ~ /Ext/) print $1 ":,"
-  else print $1 "," $2
-}
-END{
-  print "Registrant Phone Ext:,"
-  print "Registrant Fax Ext:,"
-  print "Admin Phone Ext:,"
-  print "Admin Fax Ext:,"
-  print "Tech Phone Ext:,"
-  printf "Tech Fax Ext:,"
-}
-' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{print $1","$2} END{printf "Registrant Phone Ext:,\nRegistrant Fax Ext:,\nAdmin Phone Ext:,\nAdmin Fax Ext:,\nTech Phone Ext:,\nTech Fax Ext:,"}' > "$1.csv"

aa17c1a7a590efae0b7647d3ccd71456c29ff47c
sar1yeva <yaxsisariyeva55@gmail.com>
2026-02-02 10:06:50 -0500

    cryptography_basics

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 0 -a 0 "$1" /usr/share/wordlists/rockyou.txt --force --quiet && hashcat -m 0 --show "$1" | awk -F: '{print $2}' | grep -v '^$' > 7-password.txt
+hashcat -m 0 -a 0 "$1" /usr/share/wordlists/rockyou.txt --force --quiet --outfile=7-password.txt --outfile-format=2

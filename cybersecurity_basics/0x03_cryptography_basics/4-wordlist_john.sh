a758d40bfaf3523ebc52dc8fc92c6b4c28b659f8
sar1yeva <yaxsisariyeva55@gmail.com>
2026-02-02 09:19:06 -0500

    cryptography_basics

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=Raw-SHA256 --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1 && john --format=Raw-SHA256 --show "$1" | head -n -1 | cut -d: -f2 > 4-password.txt
+john --wordlist="/usr/share/wordlists/rockyou.txt" --format=Raw-SHA256 "$1" && john --show "$1" | awk -F: '{print $2}' | grep -v '^$' > "4-password.txt"

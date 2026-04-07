eb0380ebf5256391aa2741bf0990fb9618b0e600
sar1yeva <yaxsisariyeva55@gmail.com>
2026-04-07 10:21:52 -0400

    web_application_fast_incident_response

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -6,4 +6,4 @@ log_file=${1:-logs.txt}
 attacker_ip=$(awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
 
 # Extract User-Agent used by attacker and find the most common
-awk -v ip="$attacker_ip" '$1==ip {print substr($0, index($0,$12))}' "$log_file" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+awk -v ip="$attacker_ip" '$1==ip {match($0, /"[^"]*"$/, a); print a[0]}' "$log_file" | tr -d '"' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'

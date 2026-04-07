8e3efd18e8fd9159e88eae71a29f09e75edd3d57
sar1yeva <yaxsisariyeva55@gmail.com>
2026-04-07 10:25:56 -0400

    web_application_fast_incident_response

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-log_file=${1:-logs.txt}
+# Attacker User-Agent tapmaq üçün logs.txt faylını analiz edir
+# Hər hansı IP və ya filter istifadə etmədən bütün User-Agentləri sayır
 
-# Find attacker IP
-attacker_ip=$(awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
-
-# Extract User-Agent used by attacker and find the most common
-awk -v ip="$attacker_ip" '$1==ip {match($0, /"[^"]*"$/, a); print a[0]}' "$log_file" | tr -d '"' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+# Log faylından User-Agentləri çıxarır, sayır və ən çox istifadə olunanı göstərir
+grep -o '\".*\"$' logs.txt | awk -F'"' '{print $(NF-1)}' | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'

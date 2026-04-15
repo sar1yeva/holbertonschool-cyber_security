3f62826a0f01eb53b56670972e181e815c366800
sar1yeva <yaxsisariyeva55@gmail.com>
2026-04-15 13:35:12 -0400

    web_application_foresics

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log* | grep "Accepted" | awk '{print $(NF-3)}' | sort -u | wc -l
+tail -n 1000 auth.log* | grep "Accepted password" | awk '{print $(NF-3)}' | sort -u | wc -l

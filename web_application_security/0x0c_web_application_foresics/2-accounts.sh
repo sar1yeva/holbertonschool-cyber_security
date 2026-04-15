952e0bc44ed12b8c5abe1ef89d8db5ab44b56ff4
sar1yeva <yaxsisariyeva55@gmail.com>
2026-04-15 13:28:56 -0400

    web_application_foresics

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log* | grep "Failed password for" | awk '{if($(NF-5)=="invalid"){print $(NF-4)} else {print $(NF-5)}}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+tail -n 1000 auth.log* | grep "Failed password for" | grep -v "invalid user" | awk '{print $9}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'

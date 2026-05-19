dbb5f58788df719842d74a1459a4930e2d40d9ed
sar1yeva <yaxsisariyeva55@gmail.com>
2026-05-19 07:17:11 -0400

    ruby_scripting

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -9,4 +9,6 @@ def merge_json_files(file1_path, file2_path)
   merged_data = file2_data + file1_data
 
   File.write(file2_path, JSON.pretty_generate(merged_data))
+
+  puts "Merged JSON written to file.json"
 end

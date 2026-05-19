1faac0d5992b2c8034c9760a323285359aaad1f6
sar1yeva <yaxsisariyeva55@gmail.com>
2026-05-19 07:11:24 -0400

    ruby_scripting

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'json'
+def count_user_ids(path)
+  file = File.read(path)
+  data = JSON.parse(file)
+
+  counts = Hash.new(0)
+
+  data.each do |item|
+    counts[item["userId"]] += 1
+  end
+
+  counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+end

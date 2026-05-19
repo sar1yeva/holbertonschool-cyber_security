38f4016eb066044d31234bd02af50c552443a660
sar1yeva <yaxsisariyeva55@gmail.com>
2026-05-19 07:31:46 -0400

    ruby_scripting

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,17 +1,10 @@
 #!/usr/bin/env ruby
-
 def print_arguments
   args = ARGV
 
   puts "Arguments:"
 
-  if args.empty?
-    return
-  end
-
   args.each do |arg|
     puts arg
   end
 end
-
-print_arguments
